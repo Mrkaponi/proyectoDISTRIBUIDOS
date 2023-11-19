3299e1f8d1b0e52e358d515b4f73b96f5e38be44
isaac22334455 <eliangzz44@gmail.com>
2023-11-19 10:51:36 -0600

    mis cambios dfi

--- DISTRIBUIDOS/src/main/java/com/example/distribuidos/services/SystemInfoProviderImp.java
+++ DISTRIBUIDOS/src/main/java/com/example/distribuidos/services/SystemInfoProviderImp.java
@@ -2,18 +2,27 @@ package com.example.distribuidos.services;
 
 import org.springframework.stereotype.Service;
 
+import fr.bmartel.speedtest.SpeedTestReport;
+import fr.bmartel.speedtest.SpeedTestSocket;
+import fr.bmartel.speedtest.inter.ISpeedTestListener;
+import fr.bmartel.speedtest.model.SpeedTestError;
+import oshi.SystemInfo;
+import oshi.hardware.CentralProcessor;
+
 import java.io.File;
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryMXBean;
 import java.lang.management.MemoryUsage;
 import java.lang.management.OperatingSystemMXBean;
+import java.util.concurrent.CompletableFuture;
 
 
 @Service
 public class SystemInfoProviderImp implements SystemInfoProvider{
     private final MemoryMXBean memoryMXBean;
 
-    public SystemInfoProviderImp() {
+    public SystemInfoProviderImp()
+    {
         this.memoryMXBean = ManagementFactory.getMemoryMXBean();
     }
     @Override
@@ -23,9 +32,12 @@ public class SystemInfoProviderImp implements SystemInfoProvider{
     }
 
     @Override
-    public double getProcessorSpeed() {
-
-        return 0;
+    public double getProcessorSpeed() 
+    {
+    	SystemInfo systemInfo = new SystemInfo();
+    	CentralProcessor procesador = systemInfo.getHardware().getProcessor();
+    	double velProcesador = procesador.getProcessorIdentifier().getVendorFreq() / 1e6; // MHz
+        return velProcesador;
     }
 
     @Override
@@ -85,7 +97,34 @@ public class SystemInfoProviderImp implements SystemInfoProvider{
     }
 
     @Override
-    public double getNetworkUsage() {
-        return 0;
+    public CompletableFuture<Double> getNetworkUsage() 
+    {
+        SpeedTestSocket speedTestSocket = new SpeedTestSocket();
+        CompletableFuture<Double> futuro = new CompletableFuture<>();
+		 speedTestSocket.addSpeedTestListener(new ISpeedTestListener()	
+		 
+					{
+					@Override
+				    public void onCompletion(SpeedTestReport report) {
+				        // called when download/upload is complete
+						 double anchoBanda = report.getTransferRateBit().doubleValue() / 1000000;
+						 futuro.complete(anchoBanda);
+				        System.out.println("[COMPLETED] rate in bit/s   : " + report.getTransferRateBit());
+				    }
+				    @Override
+				    public void onError(SpeedTestError speedTestError, String errorMessage) {
+				    	System.out.println("Hubo un error en la prueba de la velocidad de descarga"+ errorMessage);
+				    	System.out.println(speedTestError);
+				    	 
+				    }
+
+				    @Override
+				    public void onProgress(float percent, SpeedTestReport report) {
+				    	
+				    }
+					});	
+			speedTestSocket.startDownload("https://autogestion.metrotel.com.ar/speedtest/archivo10MB.zip",1000);
+        return futuro;
     }
 }
+
